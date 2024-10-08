46905157ae43833f0cb8c8548213f21add54882f
chezkyk <Y0548532123@GMAIL.COM>
2024-10-08 17:03:29 +0300

    added do dict function

--- models/address.py
+++ models/address.py
@@ -1,3 +1,5 @@
+from models.geo import Geo
+
 class Address:
     def __init__(self, street: str, suite: str, city: str, zipcode: str, geo: Geo):
         self.street = street
@@ -5,3 +7,11 @@ class Address:
         self.city = city
         self.zipcode = zipcode
         self.geo = geo
+    def to_dict(self):
+        return {
+            'street': self.street,
+            'suite': self.suite,
+            'city': self.city,
+            'zipcode': self.zipcode,
+            'geo': self.geo.to_dict()
+        }
