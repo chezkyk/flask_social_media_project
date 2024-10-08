1fa083c47b5342c20b1d83dd3a38acf2a05ccfbc
chezkyk <Y0548532123@GMAIL.COM>
2024-10-08 17:03:55 +0300

    added do dict function

--- models/geo.py
+++ models/geo.py
@@ -2,3 +2,9 @@ class Geo:
     def __init__(self, lat: str, lng: str):
         self.lat = lat
         self.lng = lng
+
+    def to_dict(self):
+        return {
+            'lat': self.lat,
+            'lng': self.lng
+        }
