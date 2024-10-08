8890fd7b42148a4315db03882d4524a050186fdf
chezkyk <Y0548532123@GMAIL.COM>
2024-10-08 18:39:05 +0300

    .

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
-from config import COLLECTION_NAMES,USERS_URL
-from utils.import_data_api import insert_users
+from config import COLLECTION_NAMES,USERS_URL,POSTS_URL
+from utils.import_data_api import insert_users ,insert_posts
+from blueprints.user import user_bp
+from blueprints.post import post_bp
 app = Flask(__name__)
-
+# insert_users(COLLECTION_NAMES[0], USERS_URL)
+# insert_posts(COLLECTION_NAMES[1], POSTS_URL)
+app.register_blueprint(user_bp)
+app.register_blueprint(post_bp)
 
 
 if __name__ == '__main__':
-    app.run()
-
-# insert_users(COLLECTION_NAMES[0],USERS_URL)
+    app.run(debug=True)
