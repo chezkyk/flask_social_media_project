f60eff45252f9f2d6bed173a7ae4012239728d2f
chezkyk <Y0548532123@GMAIL.COM>
2024-10-08 17:07:11 +0300

    added COLLECTION_NAMES array and url routs

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 # config.py
 MONGO_URI = 'mongodb://localhost:27017/'
 DB_NAME = 'DB1'
+COLLECTION_NAMES = ['users','posts']
+USERS_URL = 'https://jsonplaceholder.typicode.com/users'
+POSTS_URL = 'https://jsonplaceholder.typicode.com/posts'
