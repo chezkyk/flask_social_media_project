1eb825e2b449871f3fc2ecf75d9f7fd190216930
chezkyk <Y0548532123@GMAIL.COM>
2024-10-08 17:07:29 +0300

    .

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
-
+from config import COLLECTION_NAMES,USERS_URL
+from utils.import_data_api import insert_users
 app = Flask(__name__)
 
 
@@ -7,4 +8,4 @@ app = Flask(__name__)
 if __name__ == '__main__':
     app.run()
 
-# COLLECTION_NAMES = ['users','posts']
+# insert_users(COLLECTION_NAMES[0],USERS_URL)
